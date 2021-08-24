ab3b9b9a7c41c4399a043c4779e7d80f002b9e61
Andrew M Morgan <amm414@pitt.edu>
2021-08-23 22:39:23 -0400

    added history composition object

--- abm-source/ForestFireSimulation/source/FFDriver.py
+++ abm-source/ForestFireSimulation/source/FFDriver.py
@@ -1,6 +1,42 @@
 from ForestFire import ForestFireSim
 
-fire_sim = ForestFireSim(fire_spread_chance=0.5, is_print=True)
+def print_board(board):
+    string_board = ""
+    for row in board:
+        for elem in row:
+            if elem == 'Foliage':
+                string_board += "T"
+            elif elem == "Fire":
+                string_board += "F"
+            elif elem == "Dirt":
+                string_board += "."
+            elif elem == "Burnt":
+                string_board += "B"
+        string_board += "\n"
+    string_board += "\n"
+    print(string_board)
+
+
+fire_sim = ForestFireSim(fire_spread_chance=0.5, is_print=False)
 
 while fire_sim.growth_iterations < 20:
     fire_sim.simulate_iteration()
+
+print("\n\n\n\n\nTry to Print out my Own Results from Dict. Response:\n")
+
+ff_sim_history = fire_sim.history.get_dict_forest_history()
+print("Metadata:")
+for key, value in ff_sim_history['metadata']['hyper-parameters'].items():
+    print(f'{key}: {value}')
+print("Number of fires that occurred: " + str(ff_sim_history['metadata']['number_of_fires']))
+print("Number of Growth Iterations that occurred: " +
+      str(ff_sim_history['metadata']['number_of_growth_iterations']))
+
+print("\n\nStart of Printing Forest States:\n")
+print("*" * int(ff_sim_history['metadata']['hyper-parameters']['length'] +
+                ff_sim_history['metadata']['hyper-parameters']['length'] / 2))
+for entry in ff_sim_history['forest']:
+    input("Hit enter to view the next iteration...")
+    print(entry["iteration_type"] + " Iteration #" + str(entry["iteration_number"]))
+    print_board(entry['state'])
+    print("\n\n")
